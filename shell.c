7bfed71f365dc29b50b7f3519504bd9760723a45
Michael Robertson <mrobertson00@stuy.edu>
2019-12-16 03:16:12 -0500

    e

--- shell.c
+++ shell.c
@@ -118,7 +118,7 @@ void redirect (char * line, int x) {
     if(fork() == 0) {
       char ** inputs = parse_redir(line);
 
-      int file = open(inputs[1],O_CREAT|O_WRONLY, 644);
+      int file = open(inputs[1],O_CREAT|O_RDWR);
 
       char ** args = parse_args(inputs[0]);
       dup2(file,STDOUT_FILENO);
