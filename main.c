c038e46201892806615481f6ce24424096955b5f
Michael Zhang <mzhang00@stuy.edu>
2019-12-16 01:04:35 -0500

    start headers

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include "shell.h"
--- shell.c
+++ shell.c
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <dirent.h>
+#include "shell.h"
 
 char home[50];
 
