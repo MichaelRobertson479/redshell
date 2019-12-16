af1741d0fb5202541cb38a9890645d7551724530
Michael Zhang <mzhang00@stuy.edu>
2019-12-16 01:21:04 -0500

    Update main.c

--- main.c
+++ main.c
@@ -9,3 +9,38 @@
 #include <sys/wait.h>
 #include <dirent.h>
 #include "shell.h"
+
+int main(int argc, char * argv[]){
+
+  getcwd(home,50);
+  int running = 1;
+
+  while (running) {
+
+    char name[500];
+
+    fgets(name, 500, stdin);
+    int i = 0;
+    while (name[i] != '\n') {
+      i++;
+    }
+    name[i] = '\0';
+  
+    if(strstr(name, ";") != NULL) {
+      char ** commands = parse_many(name);
+
+      int i = 0;
+      while (commands[i] != NULL){
+        running = run(commands[i]);
+        i++;
+      }
+    }
+
+    else{
+      running = run(name);
+    }
+  }
+
+
+  return 0;
+}
